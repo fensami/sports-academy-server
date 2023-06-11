d8df6a7686947dad966458a88a263bf50e5f3cb9
fensami <fensami2323@gmail.com>
2023-06-12 02:00:17 +0600

    create my added classes set data mongodb

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json());
 
+
 const verifyJWT = (req, res, next) => {
   const authorization = req.headers.authorization;
   if (!authorization) {
@@ -43,6 +44,9 @@ async function run() {
     await client.connect();
 
     const usersCollection = client.db("sportsAcademy").collection("users");
+    const classesCollection = client.db('sportsAcademy').collection('classes')
+
+    const myAddClassCollection = client.db('sportsAcademy').collection('myaddedclasses');
 
 
     app.post('/jwt', (req, res) => {
@@ -62,19 +66,9 @@ async function run() {
       }
       next();
     }
-    //verify instructor
-    const verifyInstructor = async(req, res, next) => {
-      const email = req.decoded.email;
-      const query = {email: email}
-      const user = await usersCollection.findOne(query);
-      if(user?.role !== 'instructor'){
-        return res.status(403).send({error: true, message: 'forbidden message'})
-      }
-      next()
-    }
 
     // users data
-    app.get('/users',verifyJWT, verifyAdmin, async (req, res) => {
+    app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
@@ -92,10 +86,10 @@ async function run() {
       res.send(result);
     });
 
-       // security layer: verifyJWT
+    // security layer: verifyJWT
     // email same
     // check admin
-    app.get('/users/admin/:email',verifyJWT, async (req, res) => {
+    app.get('/users/admin/:email', verifyJWT, async (req, res) => {
       const email = req.params.email;
 
       if (req.decoded.email !== email) {
@@ -107,18 +101,18 @@ async function run() {
       res.send(result);
     })
     //check instructor
-    app.get('/users/instructor/:email', verifyJWT, async(req, res) => {
+    app.get('/users/instructor/:email', verifyJWT, async (req, res) => {
       const email = req.params.email;
-      if(req.decoded.email !== email){
-        res.send({admin: false})
+      if (req.decoded.email !== email) {
+        res.send({ admin: false })
       }
-      const query = {email: email}
+      const query = { email: email }
       const user = await usersCollection.findOne(query);
-      const result = {instructor: user?.role === 'instructor'}
+      const result = { instructor: user?.role === 'instructor' }
       res.send(result);
     })
 
-//set admin
+    //set admin
     app.patch('/users/admin/:id', async (req, res) => {
       const id = req.params.id;
       console.log(id);
@@ -131,19 +125,120 @@ async function run() {
       const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result);
     })
-//set instructor
-app.patch('/users/instructor/:id', async(req, res)=>{
+    //set instructor
+    app.patch('/users/instructor/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'instructor'
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result)
+    })
+
+
+    //add classes api
+    app.get('/classes', verifyJWT, async (req, res) => {
+      const result = await classesCollection.find().toArray();
+      res.send(result)
+
+    })
+    app.post('/classes', async (req, res) => {
+      const addClass = req.body;
+      // console.log(addClass);
+      const result = await classesCollection.insertOne(addClass);
+      res.send(result);
+    });
+
+    app.patch('/classes/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const classes = req.body;
+      const options = { upsert: true }
+      const updateDoc = {
+        $set: {
+          status: classes.status
+        }
+      }
+      const result = await classesCollection.updateOne(filter, updateDoc, options)
+      res.send(result)
+
+    })
+
+//add my classes collection apis
+
+// app.get('/myaddedclasses',verifyJWT, async (req, res) => {
+//       const email = req.query.email;
+//       if (!email) {
+//         res.send([]);
+//       }
+//       const decodedEmail = req.decoded.email;
+//       if (email !== decodedEmail) {
+//         return res.status(403).send({ error: true, message: 'forbidden access' })
+//       }
+      
+//       // const id = req.params.id;
+//       // const classId = req.params.id;
+//       const query = { email: email }; 
+
+//       const result = await myAddClassCollection.find(query,filter).toArray();
+//       res.send(result);
+//     });
+app.post('/myaddedclasses', async(req, res)=> {
+  const myclassAdded = req.body;
+
+  // console.log(addClass);
+  const result = await myAddClassCollection.insertOne(myclassAdded);
+  res.send(result);
+  if(req.status == 200){
+    console.log('added');
+  }
+})
+
+app.patch('/myaddedclasses/:id', async (req, res) => {
   const id = req.params.id;
-  console.log(id);
-  const filter = {_id: new ObjectId(id)};
+  const filter = { _id: new ObjectId(id) }
+  const myClasses = req.body;
+  const options = { upsert: true }
   const updateDoc = {
     $set: {
-      role: 'instructor'
-    },
-  };
-  const result = await usersCollection.updateOne(filter, updateDoc);
+      seats: myClasses.seats
+    }
+  }
+  const result = await classesCollection.updateOne(filter, updateDoc, options)
   res.send(result)
+
 })
+
+
+
+//  app.post('/myaddedclasses', async (req, res) => {
+//       const item = req.body;
+//       const result = await myAddClassCollection.insertOne(item);
+//       res.send(result.ops[0]);
+//     })
+
+    
+
+   
+
+    // app.delete('/carts/:id', async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id) };
+    //   const result = await cartCollection.deleteOne(query);
+    //   res.send(result);
+    // })
+
+
+
+
+
+
+
+
 
 
     // Send a ping to confirm a successful connection
